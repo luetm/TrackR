446e76077f2be0e94207f10cae031729da8e5dbc
Lukas Langenegger <lukas.langenegger@hexabit.ch>
2015-02-23 18:59:37 +0100

    Moved TrackR.OData to TrackR.OOData.v3

--- TrackR.OData.v3/ODataTrackRContext.cs
+++ TrackR.OData.v3/ODataTrackRContext.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TrackR.Client;
 
-namespace TrackR.OData
+namespace TrackR.OData.v3
 {
     public abstract class ODataTrackRContext : TrackRContext
     {
