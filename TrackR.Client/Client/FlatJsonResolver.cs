23b30bb461f78a28ecfb17aab151393291bcb1fc
Lukas Langenegger <lukas.langenegger@hexabit.ch>
2015-02-24 09:33:41 +0100

    Missing files.

---
+++ TrackR.Client/Client/FlatJsonResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using TrackR.Common;
+
+namespace TrackR.Client.Client
+{
+    public class FlatJsonResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty prop = base.CreateProperty(member, memberSerialization);
+
+            if (prop.PropertyType != typeof(List<EntityWrapper>) &&
+                prop.DeclaringType != typeof(EntityWrapper) &&
+                prop.PropertyType.IsClass &&
+                prop.PropertyType != typeof(string))
+            {
+                prop.ShouldSerialize = obj => false;
+            }
+
+            return prop;
+        }
+    }
+}
