8b3ab8f487afee4cc58a847ffa445a72bd295a0d
Lukas Langenegger <lukas.langenegger@hexabit.ch>
2017-06-09 08:50:43 +0200

    OnSuccess Method part 2

---
+++ TrackR.Common/HttpError.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackR.Common
+{
+    public class HttpError
+    {
+        public string Message { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string ExceptionType { get; set; }
+        public string MessageDetail { get; set; }
+        public string StackTrace { get; set; }
+
+        public HttpError InnerException { get; set; }
+    }
+}
